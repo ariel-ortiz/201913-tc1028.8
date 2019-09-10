227cf21cf6083afd01663d30d79d2afd1569737b
Ariel Ortiz <ariel.ortiz.ramirez@gmail.com>
2019-09-10 13:00:42 -0500

    Solución a tripifica y primera_palabra

--- 3. Tuplas/paractica3.py
+++ 3. Tuplas/paractica3.py
@@ -28,6 +28,44 @@ def incrementa(n):
     return (n, n_mas_1, n_mas_2, n_mas_3)
 
 
+def tripifica(tup):
+    """
+    Devuelve una tupla donde el primer elemento de tup aparece una vez, el
+    segundo elemento aparece dos veces, y el tercer elemento aparece tres
+    veces.
+
+    >>> tripifica((1, 2, 3))
+    (1, 2, 2, 3, 3, 3)
+    >>> tripifica(('a', 'b', 'c', 'd', 'e'))
+    ('a', 'b', 'b', 'c', 'c', 'c')
+    >>> tripifica(((1, 2), (3, 4), (5, 6), (7, 8)))
+    ((1, 2), (3, 4), (3, 4), (5, 6), (5, 6), (5, 6))
+    >>> isinstance(tripifica((1, 2, 3)), tuple)
+    True
+    """
+    primero = tup[0:1]
+    segundo = tup[1:2] * 2
+    tercero = tup[2:3] * 3
+    return primero + segundo + tercero
+
+
+def primera_palabra(frase):
+    """
+    Devuelve la primera palabra que se encuentra
+    en frase.
+
+    >>> primera_palabra('uno dos tres')
+    'uno'
+    >>> primera_palabra('a b c d')
+    'a'
+    >>> primera_palabra('1234 5678')
+    '1234'
+    """
+    indice_espacio = frase.find(' ')
+    palabra = frase[0:indice_espacio]
+    return palabra
+
+
 if __name__ == '__main__':
     import doctest
     doctest.testmod()
